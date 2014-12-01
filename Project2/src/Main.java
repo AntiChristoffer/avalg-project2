aefdc36c52a4bfb95b740df1ffc3a4fa3ae3ee6f
Christoffer Dahlgren <christoffer.dahlgren@gmail.com>
2014-12-01 17:01:17 +0100

    Initial commit

---
+++ Project2/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main main = new Main();
+		main.print();
+	}
+	
+	public void print(){
+		System.out.println("Hello World! Testing git from Eclipse");
+	}
+
+}
